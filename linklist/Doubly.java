568c85486847f6676181772d0dd235441380e092
karan-gander <meghawalkaran@gmail.com>
2026-04-20 08:27:18 +0530

    today

---
+++ linklist/Doubly.java
@@ -0,0 +1,124 @@
+public class Doubly {
+
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
+    };
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            size++;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+        size++;
+
+    }
+
+    public int removeFirst() {
+
+        if (head == null) {
+            System.out.print("Empty Linked List");
+            return -1;
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+
+        return val;
+
+    }
+
+    // home work to write last remove and last addd
+
+    public void addLast(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+
+            head = tail = newNode;
+            size++;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+
+            temp = temp.next;
+        }
+
+        newNode.prev = temp;
+        temp.next = newNode;
+
+    }
+
+    public int removeLast() {
+        if (head == null) {
+            System.out.print("Empty Linked List");
+            return -1;
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+    
+    }
+
+    public void print() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String args[]) {
+
+        Doubly doubleLL = new Doubly();
+
+        doubleLL.addFirst(5);
+        doubleLL.addFirst(4);
+        doubleLL.addFirst(3);
+        doubleLL.addFirst(2);
+        doubleLL.addFirst(1);
+        doubleLL.addLast(1);
+        doubleLL.addLast(4);
+
+        doubleLL.print();
+    }
+
+}
