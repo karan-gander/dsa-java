8177de81daa3fec761be0a20b1733ee408c5cebf
karan-gander <meghawalkaran@gmail.com>
2026-04-20 06:52:39 +0530

    start doubly linked list

--- linklist/Intro.java
+++ linklist/Intro.java
@@ -402,6 +402,70 @@ public class Intro {
         return merged(newLeft, newRight);
     }
 
+
+    public void zigzag(){
+
+        //find mid
+
+        Node slow = head;
+        Node fast = head.next;
+
+        while(fast!=null&&fast.next!=null){
+
+            slow=slow.next;
+            fast=fast.next.next;
+
+
+        }
+
+        Node mid = slow;
+
+
+        
+
+
+
+        //reverse 2end half 
+
+        Node prev=null;
+        Node curr = mid.next;
+        mid.next=null;
+        Node next;
+
+
+        while (curr!=null) {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+            
+        }
+
+        Node left = head;
+        Node right = prev;
+
+        Node nextL,nextR;
+
+        while (left!=null&&right!=null) {
+            
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+
+
+            right=nextR;
+            left=nextL;
+            
+            
+        }
+
+
+
+
+        // alt merge
+    }
+
     public static void main(String args[]) {
 
         Intro ll = new Intro();
@@ -441,15 +505,14 @@ public class Intro {
         
         
         
-        ll.addFirst(4);
-        ll.addFirst(3);
-        ll.addFirst(7);
-        ll.addFirst(7);
-        ll.addFirst(5);
-        ll.addFirst(0);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
         
         ll.print();
-        ll.mergedSort(head);
+        ll.zigzag();
         System.out.println();
         ll.print();
 
